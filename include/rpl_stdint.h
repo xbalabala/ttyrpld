509545f9682a537cefd681458294d0e62fb1caee
Jan Engelhardt <jengelh@computergmbh.de>
2006-06-27 20:06:34 +0000

    small note for OpenBSD <= 3.8

--- include/rpl_stdint.h
+++ include/rpl_stdint.h
@@ -33,6 +33,7 @@ include/rpl_stdint.h - STDINT wrapper
 #define RPL_STDINT_H 1
 
 #if defined(__NetBSD__)
+     // || defined(OPENBSD_3_8_AND_EARLIER)
 #    include <machine/types.h>
 #else
 #    include <stdint.h>
