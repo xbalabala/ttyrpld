c81497bfb48139cebc6ab1f68cf55a91953e4acb
Jan Engelhardt <jengelh@computergmbh.de>
2007-03-30 04:56:23 +0000

    Cast annotations

--- user/dev.h
+++ user/dev.h
@@ -1,10 +1,12 @@
 #ifndef TTYRPLD_DEV_H
 #define TTYRPLD_DEV_H 1
 
+#include "rdsh.h"
+
 #define K26_MINORBITS 20
 #define K26_MINORMASK 0xFFFFF
-#define K26_MAJOR(dev) (unsigned long)((dev) >> K26_MINORBITS)
-#define K26_MINOR(dev) (unsigned long)((dev) & K26_MINORMASK)
+#define K26_MAJOR(dev) static_cast(unsigned long, (dev) >> K26_MINORBITS)
+#define K26_MINOR(dev) static_cast(unsigned long, (dev) & K26_MINORMASK)
 #define K26_MKDEV(major, minor) \
 	(((major) << K26_MINORBITS) | ((minor) & K26_MINORMASK))
 #define KD26_PARTS(dev) K26_MAJOR(dev), K26_MINOR(dev)
@@ -17,12 +19,13 @@
  */
 #if defined(__FreeBSD__) || defined(__OpenBSD__)
 #	define COMPAT_MAJOR(dev) \
-		(unsigned long)(((dev) & 0xFF00) >> 8)
+		static_cast(unsigned long, ((dev) & 0xFF00) >> 8)
 #	define COMPAT_MINOR(dev) \
-		(unsigned long)(((dev) & 0xFF) | (((dev) & 0xFFFF0000) >> 8))
+		static_cast(unsigned long, ((dev) & 0xFF) | \
+			(((dev) & 0xFFFF0000) >> 8))
 #	define COMPAT_MKDEV(major, minor) \
-		(unsigned long)((((major) & 0xFF) << 8) | ((minor) & 0xFF) | \
-		(((minor) & 0xFFFF00) << 8))
+		static_cast(unsigned long, (((major) & 0xFF) << 8) | \
+		((minor) & 0xFF) | (((minor) & 0xFFFF00) << 8))
 
 /* Solaris
  * Scheme is:
@@ -31,11 +34,11 @@
  */
 #elif defined(__sun__)
 #	define COMPAT_MAJOR(dev) \
-		(unsigned long)(((dev) & 0xFFFC0000) >> 18)
+		static_cast(unsigned long, ((dev) & 0xFFFC0000) >> 18)
 #	define COMPAT_MINOR(dev) \
-		(unsigned long)((dev) & 0x3FFFF)
+		static_cast(unsigned long, (dev) & 0x3FFFF)
 #	define COMPAT_MKDEV(major, minor) \
-		(unsigned long)((((major) & 0x3FFF) << 18) | \
+		static_cast(unsigned long, (((major) & 0x3FFF) << 18) | \
 		((minor) & 0x3FFFF))
 
 /* Glibc-Linux and NetBSD
@@ -46,13 +49,13 @@
  */
 #else
 #	define COMPAT_MAJOR(dev) \
-		(unsigned long)(((dev) & 0xFFF00) >> 8)
+		static_cast(unsigned long, ((dev) & 0xFFF00) >> 8)
 #	define COMPAT_MINOR(dev) \
-		(unsigned long)(((dev) & 0xFF) | (((dev) & 0xFFF00000) >> 12))
+		static_cast(unsigned long, ((dev) & 0xFF) | \
+		(((dev) & 0xFFF00000) >> 12))
 #	define COMPAT_MKDEV(major, minor) \
-		(unsigned long)(((minor) & 0xFF) | \
-		(((minor) & 0xFFF00) << 12) | \
-		(((major) & 0xFFF) << 8))
+		static_cast(unsigned long, ((minor) & 0xFF) | \
+		(((minor) & 0xFFF00) << 12) | (((major) & 0xFFF) << 8))
 #endif
 
 #endif /* TTYRPLD_DEV_H */
