f0d1192cbe69b423131ab4fccacad2afd87e277b
Jan Engelhardt <jengelh@computergmbh.de>
2007-04-02 03:29:53 +0000

    Update installation instructions

--- doc/install.php
+++ doc/install.php
@@ -11,8 +11,8 @@
   <tr>
     <td class="t2"><a
       href="http://jengelh.hopto.org/coding/libHX.php">libHX</a></td>
-    <td class="t2">1.8.0</li>
-    <td class="t2">1.9.2</li>
+    <td class="t2">1.10.0</li>
+    <td class="t2">1.10.0</li>
   </tr>
   <tr>
     <td class="t1">GNU C Compiler</td>
@@ -20,32 +20,28 @@
     <td class="t1">4.x</li>
   </tr>
   <tr>
-    <td class="t2" colspan="3"><i>Auxiliary tools (i.e. unpacking,
-      documentation, etc.&nbsp;-- not strictly needed on the compiling
-      machine)</i></td>
+    <td colspan="3" class="t2"><i>If you want to recreate the
+      documentation:</i></td>
   </tr>
   <tr>
-    <td class="t1">perl</td>
+    <td class="t2">gmake (for doc)</td>
+    <td class="t2">3.79</td>
+    <td class="t2">3.81</td>
+  </tr>
+  <tr>
+    <td class="t1">perl (for doc)</td>
+    <td class="t1">-</td>
     <td class="t1">5.6.0</td>
-    <td class="t1">-</td>
   </tr>
   <tr>
-    <td class="t2">php</td>
-    <td class="t2">5.x</td>
+    <td class="t2">php (for doc)</td>
     <td class="t2">-</td>
+    <td class="t2">5.x</td>
   </tr>
   <tr>
-    <td class="t1">w3m</td>
+    <td class="t1">w3m (for doc)</td>
     <td class="t1">0.5.0</td>
-    <td class="t1">-</td>
-  </tr>
-  <tr>
-    <td class="t2">bzip2, patch, tar</td>
-    <td class="t2" colspan="2"><i>any</i></td>
-  </tr>
-  <tr>
-    <td class="t1" colspan="3"><i>Please check your kernel documentation on
-      what more tools are needed for a kernel compilation.</i></td>
+    <td class="t1">0.5.1</td>
   </tr>
 </table>
 
@@ -82,17 +78,14 @@ reboot.</p>
 You can patch your kernel source tree by issuing these commands:</p>
 
 <p class="code"><code>
-<b>$</b> cd /usr/src/linux-2.6.15/;<br />
-<b>$</b> patch -p1 -i /usr/src/ttyrpld/kpatch/linux-2.6.15.diff;<br />
+<b>$</b> cd /usr/src/linux-2.6.21-rc5/;<br />
+<b>$</b> patch -p1 -i /usr/src/ttyrpld/kpatch/linux-2.6.21-rc5.diff;<br />
 <b>$</b> make menuconfig;
 </code></p>
 
 <p class="block">Run <code>make menuconfig</code> or anything else that suits
 your needs to choose "<code>TTY logging via rpldev</code>" in the
 "<code>Security options</code>" menu.</p>
-
-<p class="block">Note: For Linux kernels 2.4.x, the "<code>TTY logging via
-rpldev</code>" option is available in the "Character devices" menu.</p>
 
 <h1>Patching the kernel sources (FreeBSD)</h1>
 
@@ -103,7 +96,7 @@ all of the exotic tty devices might be supported under FreeBSD.</p>
 
 <p class="code"><code>
 <b>$</b> cd /usr/src/sys/;<br />
-<b>$</b> patch -p1 -i /usr/src/ttyrpld/kpatch/freebsd-6.1.diff;
+<b>$</b> patch -p1 -i /usr/src/ttyrpld/kpatch/freebsd-6.2.diff;
 </code></p>
 
 <p class="block">... and rebuild the kernel image. No option needs to be
@@ -116,16 +109,13 @@ the <code>src</code> packages, and use:</p>
 <b>$</b> make kernel<br />
 </code></p>
 
-<p class="block">For <code>make kernel</code> to work, various source packages
-may need to be installed.</p>
-
 <h1>Patching the kernel sources (OpenBSD and NetBSD)</h1>
 
 <p class="block">The process is not much different:</p>
 
 <p class="code"><code>
 <b>$</b> cd /usr/src/sys/;<br />
-<b>$</b> patch -p1 -i /usr/src/ttyrpld/kpatch/openbsd-3.9.diff;
+<b>$</b> patch -p1 -i /usr/src/ttyrpld/kpatch/openbsd-4.0.diff;
 </code></p>
 
 <p class="block">Rebuild as described in <code>afterboot(8)</code>:</p>
@@ -142,25 +132,20 @@ may need to be installed.</p>
 
 <h1>Compiling the kernel module</h1>
 
+<p class="block">OpenBSD users can skip this step, as they have a monolithic
+kernel.</p>
+
 <p class="block">After having booted the new kernel, change to the appropriate
 <code>k_</code> directory for your operating system and run
 <code>make</code>.</p>
 
 <p class="block">You can tune the location of the kernel sources and module
-installation path by using <code>KERNEL_DIR=xxx</code> and/or
-<code>MODULES_DIR=xxx</code> options to <i>make</i>, respectively.</p>
-
-<p class="block">BSD users have to use <code>gmake</code>;
-<code>KERNEL_DIR</code> and <code>MODULES_DIR</code> is not applicable for BSD,
-but you may <code>cd</code> into <code>freebsd-6.1</code> by hand and run (bsd)
-<code>make</code> with any options you need. The kmod target <i>will not</i>
-install the module on FreeBSD.</p>
-
-<p class="block">OpenBSD users can skip this step, as they have a monolithic
-kernel.</p>
+installation path by passing <code>KERNEL_DIR=xxx</code> and/or
+<code>MODULES_DIR=xxx</code> parameters to <i>make</i>, respectively. After
+successful compilation, load the module.</p>
 
 <p class="block">To load the module at boot time, edit your distribution
-specific startup scripts. A hint for SUSE users: it is in
+specific startup scripts. A hint for openSUSE users: it is in
 <code>/etc/sysconfig/kernel</code> in <code>MODULES_LOADED_ON_BOOT</code>. If
 you do not know where, ask someone who knows or simply put a <code>modprobe
 rpldev</code> into <code>/etc/init.d/boot.local</code>. However, the supplied
@@ -173,43 +158,33 @@ only.</p>
 
 <h1>Compiling the user-space applications</h1>
 
-<p class="block">To compile the user-space tools, run <code>make</code> (BSD:
-<code>gmake</code>) without any arguments. This will build <code>rpld</code>
-and <code>ttyreplay</code> (the daemon and the log analyzer). If you want to
-build HTML and TXT documentation from the PHP files, you need to use
-<code>gmake doc</code>.</p>
+<p class="block">ttyrpld uses autoconf/automake (the latter of which is not
+entirely suiting my developer's needs, but whatever), so compiling and
+installing the user-space applications is a matter of running the two-step
+procedure:</p>
 
-<h1>Installing</h1>
+<p class="code"><code>./configure<br />
+make</code></p>
 
-<p class="block">A <code>make install</code> target is provided and will
-install <i>ttyrpld</i> into various places in <code>${ROOT}${PREFIX}</code>.
-This probably needs some explanation: <code>ROOT</code> is a make variable
-which is prepended to all copy operations, etc., so you can use for example
-<code>make install ROOT=/var/netboot</code> to have everything installed under
-this alternative root directory.</p>
+<p class="block">You can optionally run `<code>make install</code>`, but it is
+not strictly required.</p>
 
-<p class="block">The <code>PREFIX</code> make variable works almost the same,
-but is also used when constructing files. That is, if you run <code>make
-install PREFIX=/altdisk</code>, the init script will be adjusted to contain
-<code>/altdisk/usr/sbin/rpld</code>. By default, <code>PREFIX</code> contains
-<code>/usr/local</code>, so that binaries are installed to
-<code>/usr/local/sbin</code>. If you passed <code>PREFIX=/usr</code> instead,
-binaries would go into <code>/usr/sbin/</code>.</p>
+<h2>FreeBSD specific hacks</h2>
 
-<p class="block">Locations are:<br />
-<code>rpld</code>, <code>rplctl</code> =&gt;
-<code>${ROOT}${PREFIX}/sbin/rpld</code><br />
-<code>ttyreplay</code> =&gt; <code>${ROOT}${PREFIX}/bin</code><br />
-Locale data =&gt; <code>${ROOT}${PREFIX}/share/locale</code></p>
+<p class="block">FreeBSD does not have <code>/usr/local</code> in its default
+include and link search directory list. Meh.</p>
 
-<p class="block">The configuration file will always get installed to
-<code>${ROOT}/etc/rpld.conf</code>! If it already exists, it is not
-overwritten.</p>
+<p class="code"><code>./configure CFLAGS="-I/usr/local/include"
+LDFLAGS="-L/usr/local/lib"</code></p>
 
-<p class="block">The init script (only Linux) will always get installed to
-<code>${ROOT}/etc/init.d/</code>! However, it is not automatically activated to
-be run on bootup. Check your distribution's manual on how to do this. SUSE
-users get away with <code>insserv rpld</code>.</p>
+<h2>OpenBSD specific hacks</h2>
+
+<p class="block">Like FreeBSD, <code>/usr/local</code> is missing.
+Additionally, OpenBSD's <code>libintl.so.3.0</code> is missing a reference to
+<code>libiconv.so</code> so you have to explicitly specify it:</p>
+
+<p class="code"><code>./configure CFLAGS="-I/usr/local/include"
+LDFLAGS="-I/usr/local/lib -liconv"</code></p>
 
 <h1>Device node</h1>
 
